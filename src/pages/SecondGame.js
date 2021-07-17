9f2888d3586334f3761a14a5e043ec227d4cdf45
mateusisrael <mateuss.i.fernandes@gmail.com>
2021-07-16 23:37:05 -0300

    secondgame logic finished

--- src/pages/SecondGame.js
+++ src/pages/SecondGame.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const Content = styled.div`
@@ -24,81 +24,117 @@ const Button = styled.button`
   border-radius: 10%;
 `;
 
-function setBackground() {
-
-}
 
 export default function SecondGame() {
   const colors = {
-    crimson: "#crimson",
+    cornflowerblue: "cornflowerblue",
     aquamarine: "aquamarine",
     brown: "brown",
     tomato: "tomato"
   }
   const [brands, setBrands] = useState([])
+  const [challenger, setChallenger] = useState([
+      {id: 1, value: "1", selected: undefined},
+      {id: 4, value: "4", selected: undefined},
+      {id: 3, value: "3", selected: undefined},
+      {id: 2, value: "2", selected: undefined},
+      {id: 20, value: "2", selected: undefined},
+      {id: 10, value: "1", selected: undefined},
+      {id: 40, value: "4", selected: undefined},
+      {id: 30, value: "3", selected: undefined},
+  ]);
 
-  const challenger = {
-    colum1: [
-      {id: 1, value: "1"},
-      {id: 4, value: "4"},
-      {id: 3, value: "3"},
-      {id: 2, value: "2"},
-    ],
-    colum2: [
-      {id: 20, value: "2"},
-      {id: 10, value: "1"},
-      {id: 40, value: "4"},
-      {id: 30, value: "3"},
-    ]
-  }
+  useEffect(() => {
+    console.log(challenger)
+    console.log(brands)
+  })
 
-  function handleClick(item) {
-    addIntoBrand(item)
-  }
-
-  function addIntoBrand(item) {
-    const newBrands = brands;
-    newBrands.push(item)
-    console.log("add", newBrands)
-    setBrands([...newBrands])
-  }
-
-  function setBackground(buttonId) {
-    if(brands.length === 0) return "" 
-
-    const buttonArrayIndice = brands.findIndex(() => {return buttonId})
+  function handleClick(item, i) {
+    setBackground(item.id, i)
     
-    switch (buttonArrayIndice) {
-      case buttonArrayIndice<=2:
-        return colors.crimson;
+  }
 
-    
-      default:
-        break;
+  function setBackground(buttonId, i) {
+    const buttonArrayIndice = brands.findIndex((item) => {return item === buttonId})
+    if(buttonArrayIndice !== -1) return;
+
+    if(brands.length <= 2-1) {
+      const newChallengers = challenger
+      newChallengers[i] = {
+        ...challenger[i],
+        selected: colors.cornflowerblue
+      }
+      setBrands([...brands, buttonId]);
+      setChallenger([...newChallengers]);
+      return
     }
+
+    else if(brands.length <= 4-1) {
+      const newChallengers = challenger
+      newChallengers[i] = {
+        ...challenger[i],
+        selected: colors.aquamarine
+      } 
+      setBrands([...brands, buttonId]);
+      setChallenger([...newChallengers]);
+      return
+    }
+
+    else if(brands.length <= 6-1) {
+      const newChallengers = challenger
+      newChallengers[i] = {
+        ...challenger[i],
+        selected: colors.brown
+      } 
+      setBrands([...brands, buttonId]);
+      setChallenger([...newChallengers]);
+      return
+    }
+    
+    else if(brands.length <= 8-1) {
+      const newChallengers = challenger
+      newChallengers[i] = {
+        ...challenger[i],
+        selected: colors.tomato
+      } 
+      setBrands([...brands, buttonId]);
+      setChallenger([...newChallengers]);
+      return
+    } 
+
+    else return
+  }
+
+  function makePair() {}
+
+  function checkAnswer() {
+    if(brands.length < challenger.length) return
+    let isCorrect = true;
+    brands.map((item, i, array) => {
+      if(i%2 == 0) return
+      const par = [array[i], array[i-1]].sort()
+      // if(par[0] === par[1]-10) return isCorrect.push(true)
+      if(par[0] !== par[1]/10) return isCorrect = false;
+    });
+
+    if(isCorrect === false) {alert("Você errou")}
+    if(isCorrect === true) {alert("Você acertou")}
   }
 
   return(
-    console.log(brands),
     <>
       <h1>Second</h1>
       <Content>
         <ColumButtons>
-          {challenger.colum1.map((item) => {
+          {challenger.map((item, i) => {
             return <Button
-              backgroundColor={setBackground(item.id)}
-              onClick={() => handleClick(item)}
+              backgroundColor={item.selected}
+              onClick={() => handleClick(item, i)}
               key={item.id}>{item.value}
             </Button>
           })}
-        </ColumButtons>
-        <ColumButtons>
-          {challenger.colum2.map((item) => {
-            return <Button
-              backgroundColor={setBackground(item.id)}
-              onClick={() => handleClick(item)}
-              key={item.id}>{item.value}</Button>
-          })}
+
+          <button onClick={checkAnswer}>Checar</button>
         </ColumButtons>
       </Content>
     </>
